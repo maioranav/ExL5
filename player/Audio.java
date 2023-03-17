bc309997c4ce6e38608a19ae11f4ce2b030e6ffa
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-17 11:46:02 +0100

    audio class

---
+++ player/Audio.java
@@ -0,0 +1,35 @@
+package player;
+
+public class Audio extends MediaElement implements Playable {
+
+    private int duration = 5;
+    private int volume = 4;
+
+    public Audio(String title, int duration){
+        super(title, ElType.AUDIO);
+        this.duration = duration;
+    }
+
+    public void abbassaVolume() {
+        if (this.volume > 0) this.volume--;
+        System.out.println("Volume New = " + this.volume);
+    }
+
+    public void alzaVolume() {
+        if (this.volume < 5) this.volume++;
+        System.out.println("Volume New = " + this.volume);
+    }
+
+    public int getDuration() {
+        return this.duration;
+    }
+
+    public int getVolume() {
+        return this.volume;
+    }
+
+    public String toString() {
+        return getTitle() + getDuration();
+    }
+
+}
