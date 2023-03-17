5c900e82d0f932499ce9d0557a2b83454b250103
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-17 12:57:16 +0100

    autoplay content

--- player/Runnable.java
+++ player/Runnable.java
@@ -1,12 +1,66 @@
 package player;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Runnable {
 
+    static String tempName = "";
+    static int tempType = 0;
+    static int tempDuration = 0;
+    static int tempVol = 0;
+    static int tempLum = 0;
+    static ArrayList<MediaElement> media = new ArrayList<>();
+    static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-        Image a1 = new Image("Giovanni");
-System.out.println(a1);
-Image.show(a1);
+
+        System.out.println("\n *** PLAYER MULTIMEDIALE DI VINCENZO*** \n");
+
+        for (int e = 0; e < 5; e++) {
+            System.out.println("\n > NUOVO FILE *** \n");
+            System.out.print("Inserisci titolo: ");
+            tempName = scan.nextLine();
+            System.out.print("Inserisci tipo: \n 1- IMAGE, 2-VIDEO, 3-AUDIO 0-QUIT");
+            tempType = scan.nextInt();
+            if (tempType == 0) {
+                System.exit(0);
+            }
+            if (tempType == 1 || tempType == 2) {
+                System.out.print("Inserisci luminosità da 0 a 5:");
+                tempLum = scan.nextInt();
+            }
+            if (tempType == 2 || tempType == 3) {
+                System.out.print("Inserisci durata:");
+                tempDuration = scan.nextInt();
+                System.out.print("Inserisci volume:");
+                tempVol = scan.nextInt();
+            }
+
+            System.out.println("\n");
+
+            switch (tempType) {
+                case 1 -> media.add(new Image(tempName, tempLum));
+                case 2 -> media.add(new Video(tempName, tempDuration, tempLum, tempVol));
+                case 3 -> media.add(new Audio(tempName, 3, tempVol));
+                case 0 -> System.exit(0);
+                default -> System.out.println("Vabbè questa la duplichiamo...");
+            }
+            System.out.println("Ho inserito questo elemento nell'ARRAY!");
+
+
+        }
+
+
+        System.out.println("\n *** INIZIO A RIPRODURRE I CONTENUTI IN ORDINE *** \n");
+        
+
+        for (int i = 0; i < media.size() ; i++) {
+            switch (media.get(i).getType()) {
+                case AUDIO -> Playable.play((Audio) media.get(i));
+                case VIDEO -> Playable.play((Video) media.get(i));
+                case IMAGE -> Image.show((Image) media.get(i));
+            }
+        }
 
     }
 
