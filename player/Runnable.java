83eb16ac966078dd4be50da5b77be9898788b8e1
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-17 09:31:03 +0100

    created runnable

---
+++ player/Runnable.java
@@ -0,0 +1,11 @@
+package player;
+
+public class Runnable {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.print("Eseguito!");
+
+    }
+
+}
