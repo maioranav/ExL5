11e3a6a138941af137bccadbbabc099aeba9b4b7
Vincenzo M <vincenzomaiorana@gmail.com>
2023-03-17 11:46:14 +0100

    runnable test audio

--- player/Runnable.java
+++ player/Runnable.java
@@ -4,7 +4,9 @@ public class Runnable {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        System.out.print("Eseguito!");
+        Audio a1 = new Audio("Giovanni", 6);
+System.out.println(a1);
+Playable.play(a1);
 
     }
 
